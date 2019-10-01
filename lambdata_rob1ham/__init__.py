7226008999352284a6658b983a32e20a63a0de72
Rob Hamilton <RobHamilton3rd@gmail.com>
2019-10-01 17:16:52 -0400

    added janitor class

--- lambdata_rob1ham/__init__.py
+++ lambdata_rob1ham/__init__.py
@@ -1,8 +1,22 @@
-"""
-lambdata - a collection of data science helper functions.
-"""
-import pandas as pd
-import numpy as np
+class Janitor:
+    """
+    Janitor - a class to help with cleaning up dataframes!
+    """
+    from numpy import nan
+    import pandas as pd
 
-ONES = pd.DataFrame(np.ones(10))
-ZEROS = pd.DataFrame(np.zeros(50))
+    def __init__(self, df):
+        self.df = df
+        self.cols = df.columns
+
+    def replace_values(self, value =nan, new = -999):
+        """
+        Replaces values in Dataframe.
+        By default it will turn Default turns NaN values -999
+        """
+        self.df = self.df.replace(value, new)
+    def drop_nans(self,axis=0,how='all'):
+        """
+        This will drop rows with NaN values in a DataFrame.
+        """
+        self.df = self.df.dropna(axis,how)
